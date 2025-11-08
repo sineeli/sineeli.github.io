732789503d8305aa6dc1c34b3e740ef3ed241611
sravanneeli <sravana.neeli@gmail.com>
2025-11-08 16:09:18 -0600

    Refactor code structure for improved readability and maintainability

--- themes/icarus/layout/share/addtoany.jsx
+++ themes/icarus/layout/share/addtoany.jsx
@@ -17,11 +17,13 @@ class AddToAny extends Component {
         return <Fragment>
             <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
                 <a class="a2a_dd" target="_blank" rel="noopener" href="https://www.addtoany.com/share"></a>
+                <a class="a2a_button_linkedin"></a>
+                <a class="a2a_button_reddit"></a>
+                <a class="a2a_button_email"></a>
                 <a class="a2a_button_facebook"></a>
                 <a class="a2a_button_twitter"></a>
                 <a class="a2a_button_telegram"></a>
                 <a class="a2a_button_whatsapp"></a>
-                <a class="a2a_button_reddit"></a>
             </div>
             <script src="https://static.addtoany.com/menu/page.js"></script>
         </Fragment>;
