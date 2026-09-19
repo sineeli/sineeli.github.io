a0c5c93d8e00e38054cd55ca27308f3bd896d461
sravanneeli <sravana.neeli@gmail.com>
2026-09-19 12:46:37 -0500

    Update navbar and footer logo sizes for better visibility; adjust profile picture

--- themes/icarus/layout/common/navbar.jsx
+++ themes/icarus/layout/common/navbar.jsx
@@ -33,7 +33,7 @@ class Navbar extends Component {
             if (logo.text) {
                 navbarLogo = logo.text;
             } else {
-                navbarLogo = <img src={logoUrl} alt={siteTitle} height="28" />;
+                navbarLogo = <img src={logoUrl} alt={siteTitle} height="40" />;
             }
         } else {
             navbarLogo = siteTitle;
